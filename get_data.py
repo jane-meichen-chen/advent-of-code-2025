720859b8618928abfedabf6510c00852a888d41d
Jane Chen <jane.chen@baringa.com>
2026-08-05 15:23:26 +0100

    update get_data

--- get_data.py
+++ get_data.py
@@ -4,5 +4,4 @@ import requests
 def download_input_data(day: int, cookie: str) -> str:
     url = f"https://adventofcode.com/2025/day/{day}/input"
     response = requests.get(url, headers={"Cookie": cookie})
-    return response.text
-
+    return response.text.strip("\n")
